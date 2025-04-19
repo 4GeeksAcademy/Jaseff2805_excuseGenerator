d907222bede60e300debe321e3b9cc056eb15f1a
Omar Jaseff Bautista Bazán <40043595+jaseff2805@users.noreply.github.com>
2025-04-19 02:09:00 +0000

    Proyecto de genereación de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed on', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car', 'the TV'];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  const generateExcuse = () => {
+    const whoIndex = Math.floor(Math.random() * who.length); //Math.floor y Math.random se utilizan para la generación de números aleatorios
+    const actionIndex = Math.floor(Math.random() * action.length); //Math.floor y Math.random se utilizan para la generación de números aleatorios
+    const whatIndex = Math.floor(Math.random() * what.length); //Math.floor y Math.random se utilizan para la generación de números aleatorios
+    const whenIndex = Math.floor(Math.random() * when.length); //Math.floor y Math.random se utilizan para la generación de números aleatorios
+
+    return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`; //Sustituye a al forma básica de concatenar strings (+)
+
+  }
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
+
+
